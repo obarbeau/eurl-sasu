eba868bbeb5b0b2dc605f8b0edda547df19b091b
Olivier Barbeau <olivier.barbeau@gmail.com>
2020-08-21 15:09:51 +0200

    .

--- 2017/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testURL: 'https://www.google.com',
-  moduleFileExtensions: ["ts", "tsx", "js"],
-  testMatch: ["**/*.test.(ts|tsx|js)"],
-  transform: {
-    "^.+\\.ts?$": "ts-jest"
-  }
-};
